46392ac3451d3762c087381ef145e49259b3e466
jerlinsteffi <steffiarulsamy@gmail.com>
2026-06-04 13:53:33 +0530

    admin products and api

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 // src/lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
