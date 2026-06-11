efdc03baa8fa281fd76c253919733e2de4cb4bce
Manishasubburajs <manisha@tito.org.in>
2026-06-11 19:15:22 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,13 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth();
 
 export const config = {
-  matcher: ["/admin/dashboard/:path*", "/admin/orders/:path*", "/admin/products/:path*", "/admin/broadcast/:path*", "/admin/customers/:path*"],
+  matcher: [
+    "/admin/dashboard/:path*",
+    "/admin/orders/:path*",
+    "/admin/products/:path*",
+    "/admin/broadcast/:path*",
+    "/admin/customers/:path*",
+  ],
 };
