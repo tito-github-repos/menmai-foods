a01fcf15cce25504108e0a28654dbb6838d3b1c3
Manishasubburajs <manisha@tito.org.in>
2026-06-14 10:11:12 +0530

    product psge

--- src/app/api/enquiry/route.ts
+++ src/app/api/enquiry/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const body = await req.json();
 
