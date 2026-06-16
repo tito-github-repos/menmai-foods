8a73ba66ec33f74f5985d889061bd148590df854
Manishasubburajs <manisha@tito.org.in>
2026-06-16 08:15:49 +0530

    next congif and sidebar

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // ✅ fixes local /logo.jpeg on subdomain
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "admin.menmaifoods.com",
-      },
-      {
-        protocol: "https",
-        hostname: "menmaifoods.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.menmaifoods.com",
-      },
+      { protocol: "https", hostname: "admin.menmaifoods.com" },
+      { protocol: "https", hostname: "menmaifoods.com" },
+      { protocol: "https", hostname: "www.menmaifoods.com" },
     ],
   },
 };
